b5b723e46ade70778ca5bc21093121b645389eb8
ankitkaushik2004 <ankitkaushik7868@gmail.com>
2026-03-20 12:45:56 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom"
 import Home from './pages/Home'
-import UseGetCurrentUser from './hooks/UseGetCurrentUser'
-export const serverUrl="http://localhost:5000"
+import useGetCurrentUser from './hooks/useGetCurrentUser'
+{/*import { useSelector } from 'react-redux'
+import Dashboard from './pages/Dashboard'
+import Generate from './pages/Generate'
+import WebsiteEditor from './pages/Editor'
+import LiveSite from './pages/LiveSite'
+import Pricing from './pages/Pricing' */}
 
-const App = () => {
-  UseGetCurrentUser()
+export const serverUrl="http://localhost:8000"
+function App() {
+  useGetCurrentUser()
+//  const {userData}=useSelector(state=>state.user)
   return (
    <BrowserRouter>
-   <Routes >
-    <Route path="/" element={<Home/>}/>
-
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+ {/*}   <Route path='/dashboard' element={userData?<Dashboard/>:<Home/>}/>
+     <Route path='/generate' element={userData?<Generate/>:<Home/>}/>
+     <Route path='/editor/:id' element={userData?<WebsiteEditor/>:<Home/>}/>
+      <Route path='/site/:id' element={<LiveSite/>}/>
+       <Route path='/pricing' element={<Pricing/>}/> */}
    </Routes>
    </BrowserRouter>
   )
