a07dee04df714da47dcf38df50113ca6715ee268
ankitkaushik2004 <ankitkaushik7868@gmail.com>
2026-03-20 13:05:51 +0530

    Update store.js

--- store.js
+++ store.js
@@ -1,24 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userSlice from "./userSlice"
-
-export const store = configureStore({
-    reducer :{ // is reducer ka andar hi ham sare slices rakhta h 
-        user:userSlice
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    } 
+export const store=configureStore({
+    reducer:{
+      user:userSlice
+    }
 })
